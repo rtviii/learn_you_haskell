2c3cab1a12849158267d897fbef23b0a90400e13
Artem <rtkushner@gmail.com>
2022-08-28 22:31:11 -0700

    typeclasses

--- hello.hs
+++ hello.hs
@@ -1,6 +1,5 @@
 doubleMe x = x + x  
 
 doubleUs x y = doubleMe x + doubleMe y  
-
 comprehension=[x*2 | x <- [1..10], x*2 >= 12]  
 rightTriangles'  perimeter = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == perimeter]  
---
+++ typesystem.hs
@@ -0,0 +1,4 @@
+-- http://learnyouahaskell.com/types-and-typeclasses
+
+removeNonUppercase :: [Char]->[Char]
+removeNonUppercase stringy = [ c | c <-stringy, c `elem` ['A'..'Z']]
