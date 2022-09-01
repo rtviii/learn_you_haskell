31661e85af274b7e7c9e2a125257db9e65d48a88
Artem <rtkushner@gmail.com>
2022-08-31 20:39:49 -0700

    matching

--- functions.hs
+++ functions.hs
@@ -1,1 +1,12 @@
 --  Pattern matching
+lucky :: (Integral a) => a -> String  
+lucky 7 = "LUCKY NUMBER SEVEN!"  
+lucky x = "Sorry, you're out of luck, pal!"   
+
+factorial :: (Integral a) => a -> a  
+factorial 0 = 1  
+factorial n = n * factorial (n - 1)  
+
+islong :: [a] -> String  
+islong [] = "Empty"
+islong l =  if length l > 4 then "long" else "not so long"
